21b52840cb06a4e7261fe6cb19fdef31bcbeb0c6
manuelguido <manuelguido.gb@gmail.com>
2023-08-25 19:03:02 -0300

    Removes @vite css import

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,10 +9,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        @vite('resources/css/app.css ')
     </head>
     <body>
         <div id="app"></div>
-        @vite('resources/js/app.js ')
+        @vite('resources/js/app.js')
     </body>
 </html>
