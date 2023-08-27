610127de2c88f977eebc136d02a1959369920669
manuelguido <manuelguido.gb@gmail.com>
2023-08-27 04:21:42 -0300

    Add vuetify material design icons and extend vuetify configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,9 +7,31 @@ import router from './router/index.js'
 import store from './store';
 
 // Vuetify setup
-import { createVuetify } from 'vuetify'
-const vuetify = createVuetify()
-import 'vuetify/styles'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+import { fa } from "vuetify/iconsets/fa";
+import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
+import "@mdi/font/css/materialdesignicons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: "light",
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+      fa,
+    },
+  },
+  components,
+  directives,
+});
 
 // Vue Application setup
 createApp(app).use(router).use(store).use(vuetify).mount("#app")
