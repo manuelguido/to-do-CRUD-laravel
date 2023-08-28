b462e6e4fee16df5be2457ecd265a51aabd54685
manuelguido <manuelguido.gb@gmail.com>
2023-08-28 14:57:48 -0300

    Replace axios call with apiMixin on logout button component

--- resources/js/components/navigation/nav-bar/logout-button.vue
+++ resources/js/components/navigation/nav-bar/logout-button.vue
@@ -1,36 +1,33 @@
 <template>
 	<form @submit.prevent="logout">
-		<v-btn color="teal-darken-4" type="submit" variant="outlined" append-icon="mdi-logout" size="large">{{ buttonText }}</v-btn>
+		<v-btn color="teal-darken-4" type="submit" variant="outlined" append-icon="mdi-logout" size="large">{{ buttonText
+		}}</v-btn>
 	</form>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import axios from "axios";
+import apiMixin from "../../../mixins/apiMixin";
 
 export default defineComponent({
+	mixins: [apiMixin],
 	data() {
 		return {
 			buttonText: "Logout" as string,
 		};
 	},
 	methods: {
-		async logout() {
-			try {
-				const token = this.$store.getters["auth/getToken"];
+		async logout(): Promise<any> {
+			const url: string = "/api/logout";
 
-				await axios.post("/api/logout", {}, {
-					headers: {
-						Authorization: `Bearer ${token}`,
-					},
-				});
-
-				await this.$store.dispatch("auth/logout");
-
-				this.$router.push("/login");
-			} catch (error) {
-				console.error("Logout error:", error);
-			}
+			this.$httpPost(url)
+				.then(() => {
+					this.$store.dispatch("auth/logout");
+					this.$router.push("/login");
+				})
+				.catch((error) => {
+					console.log(error)
+				})
 		},
 	},
 });
