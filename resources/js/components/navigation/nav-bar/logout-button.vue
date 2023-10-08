18fedee2ead08a5df028783d21159bc1975d3a42
manuelguido <manuelguido.gb@gmail.com>
2023-10-08 17:43:13 -0300

    Fix: logout button not dispaching auth/logout on catch()

--- resources/js/components/navigation/nav-bar/logout-button.vue
+++ resources/js/components/navigation/nav-bar/logout-button.vue
@@ -1,8 +1,14 @@
 <template>
-	<form @submit.prevent="logout">
-		<v-btn color="teal-darken-4" type="submit" variant="outlined" append-icon="mdi-logout" size="large">{{ buttonText
-		}}</v-btn>
-	</form>
+    <form @submit.prevent="logout">
+        <v-btn
+            color="teal-darken-4"
+            type="submit"
+            variant="outlined"
+            append-icon="mdi-logout"
+            size="large"
+            >{{ buttonText }}</v-btn
+        >
+    </form>
 </template>
 
 <script lang="ts">
@@ -10,25 +16,20 @@ import { defineComponent } from "vue";
 import apiMixin from "../../../mixins/apiMixin";
 
 export default defineComponent({
-	mixins: [apiMixin],
-	data() {
-		return {
-			buttonText: "Logout" as string,
-		};
-	},
-	methods: {
-		async logout(): Promise<any> {
-			const url: string = "/api/logout";
+    mixins: [apiMixin],
+    data() {
+        return {
+            buttonText: "Logout" as string,
+        };
+    },
+    methods: {
+        async logout(): Promise<any> {
+            const url: string = "/api/logout";
 
-			this.$httpPost(url)
-				.then(() => {
-					this.$store.dispatch("auth/logout");
-					this.$router.push("/login");
-				})
-				.catch((error) => {
-					console.log(error)
-				})
-		},
-	},
+            this.$httpPost(url);
+            this.$store.dispatch("auth/logout");
+            this.$router.push("/login");
+        },
+    },
 });
 </script>
